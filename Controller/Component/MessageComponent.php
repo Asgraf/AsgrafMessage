2a554a33728283193ce82d6fd6e86203f04b4703
Michal Asgraf Turek <asgraf@gmail.com>
2013-05-09 12:57:14 +0200

    Initial commit

---
+++ Controller/Component/MessageComponent.php
@@ -0,0 +1,31 @@
+<?php
+App::uses('Component', 'Controller');
+/**
+ * Sends message to user and redirects using proper format
+ * @author Michal Turek <asgraf@gmail.com>
+ * @link https://github.com/Asgraf/AsgrafMessage
+ */
+class MessageComponent extends Component {
+	/**
+	 * @var Controller
+	 */
+	private $controller = null;
+	
+	public function initialize(Controller $controller) {
+		$this->controller = $controller;
+	}
+	
+	public function flash($msg,$url=null,$type='neutral',$metadata=array()) {
+		if(empty($this->controller->request->params['ext'])) {
+			if(!empty($this->controller->Session)) {
+				$this->controller->Session->setFlash($msg,'default',array(),$type);
+				if(!empty($url)) $this->controller->redirect($url);
+			} else {
+				$this->controller->flash($msg,$url);
+			}
+		} else {
+			$this->controller->set('responce',array_merge(array('message'=>$msg,'redirect'=>Router::url($url),'type'=>$type),$metadata));
+			$this->controller->set('_serialize','responce');
+		}
+	}
+}
